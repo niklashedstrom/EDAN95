63c6862d2217132ef7f50675aecf7d36fa7b3eb6
danielaahlin <daniel.ahlin96@gmail.com>
2019-11-21 13:58:00 +0100

    pang

--- lab3/imagemodel.py
+++ lab3/imagemodel.py
@@ -1,5 +1,7 @@
-from keras import layers
-from keras import models
+from keras import layers, models
+from keras.preprocessing.image import ImageDataGenerator
+from keras import optimizers
+
 import os, shutil
 
 
@@ -17,10 +19,34 @@ if __name__ == "__main__":
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Flatten())
     model.add(layers.Dense(512, activation='relu'))
-    model.add(layers.Dense(1, activation='sigmoid'))
+    model.add(layers.Dense(5, activation='softmax'))
 
-    
+    model.compile(loss='categorical_crossentropy',
+    optimizer=optimizers.RMSprop(lr=1e-4),
+    metrics=['acc'])
+
 
     train_datagen = ImageDataGenerator(rescale=1. / 255)
     val_datagen = ImageDataGenerator(rescale=1. / 255)
     test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+    train_generator = train_datagen.flow_from_directory(
+    'flowers_split/train',
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='categorical')
+
+    validation_generator = test_datagen.flow_from_directory(
+    'flowers_split/validation',
+    target_size=(150, 150),
+    batch_size=20,
+    class_mode='categorical')
+
+    history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=100,
+    epochs=30,
+    validation_data=validation_generator,
+    validation_steps=50)
+
+    model.save('flowers.h5')
