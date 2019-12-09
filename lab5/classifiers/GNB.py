13d6beff5a7c093526b0e6482fcf1513472a812c
danielaahlin <daniel.ahlin96@gmail.com>
2019-12-09 09:26:17 +0100

    FCK OGFF

--- lab5/classifiers/GNB.py
+++ lab5/classifiers/GNB.py
@@ -4,26 +4,25 @@ Gaussian Naive Bayesian Classifier
 
 from collections import Counter
 import numpy as np
+import math
 
 class GNB:
     def __init__(self):
         self.prior = {}
-        self.index_posterior = {}   # From index feature[0] -> feature[63]
-        self.mean = 0
-        self.variance = 0
+        self.index_posterior = {}
+        self.means = []
+        self.variances = []
 
     def fit(self, features, labels):
         classes = list(set(labels))
-        counter = Counter(labels)
-        total = len(labels)
-        self.prior = {c: counter[c]/total for c in classes}
-
-        self.mean, self.variance = self.mean_var(np.array(features), np.array(labels), len(classes))
+        freq_per_class = Counter(labels)
+        self.prior = {c : freq_per_class[c]/len(labels) for c in classes}
 
         index_length = len(features[0])
         entire_list = [x for y in features for x in y]
-        uniq_vals = list(set(entire_list))
-        self.index_posterior = {i : {c : {v : 0 for v in uniq_vals} for c in classes} for i in range(index_length)}
+        attributes = list(set(entire_list))
+
+        self.index_posterior = {i : {c : {v : 0 for v in attributes} for c in classes} for i in range(index_length)}
 
         for i in range(len(labels)):
             in_class = labels[i] # Column
@@ -31,11 +30,23 @@ class GNB:
             for j in range(len(features[i])): # 0 - 63 (What matrix)
                 val = features[i][j] # Row
                 self.index_posterior[j][in_class][val] += 1
+        
+        epsilon = 0.16
+        for c in self.prior:
+            X = np.array([np.array(features[i]) for i in range(len(features)) if labels[i] == c])
+            X = np.transpose(X)
+            tmp_means = []
+            tmp_vars = []
+            for x in X:
+                tmp_means.append(np.mean(x))
+                tmp_vars.append(np.var(x) + epsilon)
+            self.means.append(tmp_means)
+            self.variances.append(tmp_vars)
 
         for matrix in self.index_posterior:
             for c in self.index_posterior[matrix]:
                 for v in self.index_posterior[matrix][c]:
-                    self.index_posterior[matrix][c][v] = self.index_posterior[matrix][c][v] / counter[c]
+                    self.index_posterior[matrix][c][v] = self.index_posterior[matrix][c][v] / freq_per_class[c]
 
     def predict(self, features):
         predict = []
@@ -45,41 +56,18 @@ class GNB:
                 val_vector = []
                 for i in range(len(f)): # i vilken matris
                     x_i = self.index_posterior[i][c][f[i]]
-                    v = self.variance[c][i] + 0.1
-                    m = self.mean[c][i]
-                    gauss = (1 / np.sqrt(2*np.pi * v**2)) ** (-(((x_i - m)**2) / (2*(v**2))))
+                    v = self.variances[c][i] 
+                    m = self.means[c][i]
+                    gauss = 1.0 / np.sqrt(2*np.pi*v) * np.exp(-1.0 * math.pow(m - f[i], 2) / v)
                     val_vector.append(gauss)
                 val_vector.append(self.prior[c])
                 mul_sum = np.prod(val_vector)
                 prob_per_class.append(mul_sum)
-            max_prob = max(prob_per_class)
-            max_class = prob_per_class.index(max_prob)
-            predict.append(max_class)
+            summ = sum(prob_per_class)
+            if summ > 0.0:
+                prob_per_class = prob_per_class / summ
+                max_class = np.argmax(prob_per_class)
+                predict.append(max_class)
+            else:
+                predict.append(10)
         return predict
-    
-    def mean_var(self, X, y, nbr_classes):
-        n_features = X.shape[1]
-        m = np.ones((nbr_classes, n_features))
-        v = np.ones((nbr_classes, n_features))
-        n_0 = np.bincount(y)[np.nonzero(np.bincount(y))[0]][0]
-        x0 = np.ones((n_0, n_features))
-        x1 = np.ones((X.shape[0] - n_0, n_features))
-        
-        k = 0
-        for i in range(0, X.shape[0]):
-            if y[i] == 0:
-                x0[k] = X[i]
-                k = k + 1
-        k = 0
-        for i in range(0, X.shape[0]):
-            if y[i] == 1:
-                x1[k] = X[i]
-                k = k + 1
-            
-        for j in range(0, n_features):
-            m[0][j] = np.mean(x0.T[j])
-            v[0][j] = np.var(x0.T[j])*(n_0/(n_0 - 1))
-            m[1][j] = np.mean(x1.T[j])
-            v[1][j] = np.var(x1.T[j])*((X.shape[0]-n_0)/((X.shape[0]
-                                                        - n_0) - 1))
-        return m, v # mean and variance 
--- lab5/main.py
+++ lab5/main.py
@@ -2,6 +2,8 @@ from classifiers.NCC import NCC
 from classifiers.NBC import NBC
 from classifiers.GNB import GNB
 
+from sklearn.naive_bayes import GaussianNB
+
 from sklearn import metrics
 from sklearn import datasets
 
@@ -39,14 +41,14 @@ def main():
     digits = datasets.load_digits()
 
     split = int(0.7 * digits.data.shape[0])
-    train_feature = digits.data[:split].tolist()
-    train_label = digits.target[:split].tolist()
+    train_feature = digits.data[:split]
+    train_label = digits.target[:split]
     test_feature = digits.data[split:]
     test_labels = digits.target[split:]
 
     # SciKitLearn digits summarised (2.2)
     modified_digits_data = modify_data(digits.data)
-    modified_digits_labels = modify_target(digits.target)
+    modified_digits_labels = digits.target
 
     train_feature_mod = modified_digits_data[:split]
     train_label_mod = modified_digits_labels[:split]
@@ -58,33 +60,40 @@ def main():
     mnist = MNIST.MNISTData('MNIST_Light/*/*.png')
     train_features_mnist, test_features_mnist, train_labels_mnist, test_labels_mnist = mnist.get_data()
 
-    gnb = GNB()
+    # gnb = GNB()
+    # gnb.fit(train_feature, train_label)
+    # y_pred = gnb.predict(test_feature)
+    gnb = GaussianNB()
     gnb.fit(train_feature, train_label)
     y_pred = gnb.predict(test_feature)
 
-    # ncc = NCC()
-    # ncc.fit(train_feature, train_label)
-    # y_pred = ncc.predict(test_feature)
+    print("Classification report SKLearn GNB:\n%s\n"
+      % (metrics.classification_report(test_labels, y_pred)))
+    print("Confusion matrix SKLearn GNB:\n%s" % metrics.confusion_matrix(test_labels, y_pred))
+
+    ncc = GNB()
+    ncc.fit(train_feature, train_label)
+    y_pred = ncc.predict(test_feature)
 
     print("Classification report NCC (dataset 2.1):\n%s\n"
       % (metrics.classification_report(test_labels, y_pred)))
     print("Confusion matrix NCC:\n%s" % metrics.confusion_matrix(test_labels, y_pred))
 
-    # ncc2 = NCC()
-    # ncc2.fit(train_feature_mod, train_label_mod)
-    # y_pred_mod = ncc2.predict(test_feature_mod)
+    ncc2 = GNB()
+    ncc2.fit(train_feature_mod, train_label_mod)
+    y_pred_mod = ncc2.predict(test_feature_mod)
 
-    # print("Classification report NCC (dataset 2.2):\n%s\n"
-    #   % (metrics.classification_report(test_label_mod, y_pred_mod)))
-    # print("Confusion matrix NCC:\n%s" % metrics.confusion_matrix(test_label_mod, y_pred_mod))
+    print("Classification report NCC (dataset 2.2):\n%s\n"
+      % (metrics.classification_report(test_label_mod, y_pred_mod)))
+    print("Confusion matrix NCC:\n%s" % metrics.confusion_matrix(test_label_mod, y_pred_mod))
 
-    # ncc3 = NCC()
-    # ncc3.fit(train_features_mnist, train_labels_mnist)
-    # y_pred_mnist = ncc3.predict(test_features_mnist)
+    ncc3 = GNB()
+    ncc3.fit(train_features_mnist, train_labels_mnist)
+    y_pred_mnist = ncc3.predict(test_features_mnist)
 
-    # print("Classification report NCC (dataset 2.3):\n%s\n"
-    #   % (metrics.classification_report(test_labels_mnist, y_pred_mnist)))
-    # print("Confusion matrix NCC:\n%s" % metrics.confusion_matrix(test_labels_mnist, y_pred_mnist))
+    print("Classification report NCC (dataset 2.3):\n%s\n"
+      % (metrics.classification_report(test_labels_mnist, y_pred_mnist)))
+    print("Confusion matrix NCC:\n%s" % metrics.confusion_matrix(test_labels_mnist, y_pred_mnist))
 
 if __name__ == "__main__":
     main()
