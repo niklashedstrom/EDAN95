c83ff75164ad1e6f8a0450f661aa7dda5e19fef5
danielaahlin <daniel.ahlin96@gmail.com>
2019-12-09 16:58:13 +0100

    looks like something

--- lab6/EM.py
+++ lab6/EM.py
@@ -2,7 +2,7 @@ from collections import Counter
 from scipy.stats import multivariate_normal
 
 import numpy as np
-import math 
+import math
 
 class EM:
     def __init__(self):
@@ -18,6 +18,8 @@ class EM:
             mu = np.random.rand(1, 64) * 16
             std = np.random.rand(1, 64) * 16
             theta.append((1/len(classes), list(map(int, mu[0])), list(std[0])))
+        
+        # for 50 iterations: 
 
         """
         E-step
@@ -30,14 +32,19 @@ class EM:
             for k in classes:
                 tmp_ri.append( (theta[k][0] * self.P(features[i], theta[k])) / denominatior )
             r_ik.append(tmp_ri)
-        print(r_ik)
+
+        """
+        M-step
+        """
+
+        r_k = []
+        for k in classes:
+            r_k.append(sum(r_ik[i][k] for i in range(len(features))))
+
+        print(r_k)
 
     def P(self, xi, theta_k):
         prod = 1.0
         for j in range(64):
-            print('for values: mu: {}, std: {}, xi: {}'.format(theta_k[1][j], theta_k[2][j], xi[j]))
-            print(1 / np.sqrt(2*np.pi*(theta_k[2][j]**2)))
-            print(np.exp( - (((xi[j] - theta_k[1][j])**2) / (2*(theta_k[2][j]**2))) ))
-            print(prod)
-            print(20 * '-')
-            prod *= (1 / np.sqrt(2*np.pi*(theta_k[2][j]**2)) * np.exp( - (((xi[j] - theta_k[1][j])**2) / (2*(theta_k[2][j]**2))) ))
+            prod *= (1 / np.sqrt(2*np.pi*(theta_k[2][j]**2)) * np.exp( - (((xi[j] - theta_k[1][j])**2) / (2*(theta_k[2][j]**2))) )) 
+        return prod
